05dc08874f22a542372891ea8b829f2916e58dfc
cheykamp2058 <cheykamp2058@student.pps.net>
2023-05-12 09:48:37 -0700

    Finished with input boxes, need to assign input to a variable.

--- IndependentProject.java
+++ IndependentProject.java
@@ -41,6 +41,7 @@ public class IndependentProject implements ActionListener {
     JButton circle = new JButton("Circular Graph: r² = √((x - a)² + (y - b)²)"); // Creates circular graph button
     JButton trig = new JButton("Trigonometric Function: y = a(sin/cos/tan)bx"); // Creates graphing button
     JTextField label = new JTextField("Choice what kind of equation you want to graph");//Sets the label/directions at the top of page
+
     //Creates windows for input for functions
     JFrame verticalWindow = new JFrame("Vertical");
     JFrame horizontalWindow = new JFrame("Horizontal");
@@ -69,6 +70,37 @@ public class IndependentProject implements ActionListener {
         quadraticDerivativeWindow.setSize(200,400);
         circularWindow.setSize(200,400);
         trigWindow.setSize(200,400);
+        verticalWindow.setLayout(new BorderLayout());
+        horizontalWindow.setLayout(new BorderLayout());
+        linearWindow.setLayout(new GridLayout(2, 1));
+        quadraticWindow.setLayout(new GridLayout(3, 1));
+        linearDerivativeWindow.setLayout(new GridLayout(2, 1));
+        quadraticDerivativeWindow.setLayout(new GridLayout(3, 1));
+        circularWindow.setLayout(new GridLayout(2, 1));
+        trigWindow.setLayout(new GridLayout(2, 1));
+        verticalWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        horizontalWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        linearWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        quadraticWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        linearDerivativeWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        quadraticDerivativeWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        circularWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        trigWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+        //Unused containers for pop-ups
+        Container linearContainer = new Container();
+        Container quadraticContainer = new Container();
+        Container circularContainer = new Container();
+        Container trigContainer = new Container();
+        linearWindow.add(linearContainer);
+        quadraticWindow.add(quadraticContainer);
+        linearDerivativeWindow.add(linearContainer);
+        quadraticDerivativeWindow.add(quadraticContainer);
+        circularWindow.add(circularContainer);
+        trigWindow.add(trigContainer);
+         */
+
+
 
         // Sets up JFrame window:
         JFrame window = new JFrame("Calculator");
@@ -81,17 +113,6 @@ public class IndependentProject implements ActionListener {
         mainContainer.setLayout(new GridLayout(4, 2));
         window.add(labelContainer, BorderLayout.NORTH);
         window.add(mainContainer, BorderLayout.CENTER);
-        /* work in progress to add input boxes to the frames that show up when a button is pressed
-        JTextField x = new JTextField();
-        JTextField b = new JTextField();
-        JTextField m = new JTextField();
-        JTextField c = new JTextField();
-        x.setFont(new Font("Sansferif", Font.PLAIN, 42));
-        b.setFont(new Font("Sansferif", Font.PLAIN, 42));
-        m.setFont(new Font("Sansferif", Font.PLAIN, 42));
-        c.setFont(new Font("Sansferif", Font.PLAIN, 42));
-
-        */
 
         // Adds buttons to window in specific order:
         mainContainer.add(vertical);
@@ -130,28 +151,55 @@ public class IndependentProject implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JTextField x = new JTextField("x = ");
+        JTextField b = new JTextField("b = ");
+        JTextField m = new JTextField("m = ");
+        JTextField c = new JTextField("c = ");
+        JTextField a = new JTextField("a = ");
+        x.setFont(new Font("Sansferif", Font.PLAIN, 30));
+        b.setFont(new Font("Sansferif", Font.PLAIN, 30));
+        m.setFont(new Font("Sansferif", Font.PLAIN, 30));
+        c.setFont(new Font("Sansferif", Font.PLAIN, 30));
+        a.setFont(new Font("Sansferif", Font.PLAIN, 30));
+
         if(e.getSource().equals(vertical)){
+            verticalWindow.add(c, BorderLayout.CENTER);
             verticalWindow.setVisible(true);
         }
         else if(e.getSource().equals(horizontal)){
+            horizontalWindow.add(c, BorderLayout.CENTER);
             horizontalWindow.setVisible(true);
         }
         else if(e.getSource().equals(linear)){
+            linearWindow.add(m, BorderLayout.NORTH);
+            linearWindow.add(b,BorderLayout.NORTH);
             linearWindow.setVisible(true);
         }
         else if(e.getSource().equals(quadratic)){
+            quadraticWindow.add(a, BorderLayout.NORTH);
+            quadraticWindow.add(b, BorderLayout.CENTER);
+            quadraticWindow.add(c, BorderLayout.SOUTH);
             quadraticWindow.setVisible(true);
         }
         else if(e.getSource().equals(linearDerivative)){
+            linearDerivativeWindow.add(m, BorderLayout.NORTH);
+            linearDerivativeWindow.add(b, BorderLayout.CENTER);
             linearDerivativeWindow.setVisible(true);
         }
         else if(e.getSource().equals(quadraticDerivative)){
+            quadraticDerivativeWindow.add(a, BorderLayout.NORTH);
+            quadraticDerivativeWindow.add(b, BorderLayout.CENTER);
+            quadraticDerivativeWindow.add(c, BorderLayout.SOUTH);
             quadraticDerivativeWindow.setVisible(true);
         }
         else if(e.getSource().equals(circle)){
+            circularWindow.add(a);
+            circularWindow.add(b);
             circularWindow.setVisible(true);
         }
         else if(e.getSource().equals(trig)){
+            trigWindow.add(a);
+            trigWindow.add(b);
             trigWindow.setVisible(true);
         }
 
