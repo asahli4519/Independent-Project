d4348d8f502a0a1924f1dadc556b76263d63e49a
asahli4519 <asahli4519@student.pps.net>
2023-05-16 10:00:30 -0700

    Made progress on the graph today, need to get the scale right next class.

--- GraphingPlane.java
+++ GraphingPlane.java
@@ -20,22 +20,37 @@ class MyPanel extends JPanel {
     private final int POINTWIDTH = 3;
     private final int POINTHEIGHT = 3;
 
-    private  int xPos = 20;
-
-    private  int yPos = 20;
-
     private int color = 0;
+
+    private int scale = 20;
+
+    private int dimensions = 960; // Size of graph plane. 960 = 12 x 80
 
     public MyPanel() {
         setBorder(BorderFactory.createLineBorder(Color.black));
     }
 
     public Dimension getPreferredSize() {
-        return new Dimension(1000,1000);
+        return new Dimension(dimensions,dimensions);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        for (int i = 0; i < dimensions; i++) {
+            g.fillRect(dimensions / 2, i, 1, 1);
+            g.drawRect(dimensions / 2, i, 1, 1);
+        }
+
+        for (int i = 0; i < dimensions; i++) {
+            g.fillRect(i, dimensions / 2, 1, 1);
+            g.drawRect(i, dimensions / 2, 1, 1);
+        }
+        g.drawString("10", dimensions / 2 + 15, 15); // North
+        g.drawString("10", dimensions - 20, dimensions / 2 + 15); // East
+        g.drawString("-10", dimensions / 2 + 15, dimensions - 5); // South
+        g.drawString("-10", 5, dimensions / 2 + 15); // West
+
         if (color == 1) {
             g.setColor(Color.RED);
         }
@@ -51,7 +66,11 @@ class MyPanel extends JPanel {
         else {
             g.setColor(Color.BLACK);
         }
-        g.fillRect(xPos, yPos, POINTWIDTH, POINTHEIGHT);
-        g.drawRect(xPos, yPos, POINTWIDTH, POINTHEIGHT);
+        double point;
+        for (int x = 0; x < dimensions; x++) {
+            point = x - (double)(dimensions / 2);
+            g.fillRect((x - x / 2), dimensions - (x * x) / scale, POINTWIDTH, POINTHEIGHT);
+            g.drawRect((x - x / 2), dimensions - (x * x) / scale, POINTWIDTH, POINTHEIGHT);
+        }
     }
 }
