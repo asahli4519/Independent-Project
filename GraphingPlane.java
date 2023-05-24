4d07ead512886ca368f9ef15f21641f62385c5f9
asahli4519 <asahli4519@student.pps.net>
2023-05-24 09:03:50 -0700

    Test

--- GraphingPlane.java
+++ GraphingPlane.java
@@ -27,28 +27,28 @@ class MyPanel extends JPanel {
     // Variables for individual graphs:
 
         // Vertical line:
-        private int vc;
+        private int vc = 3;
 
         // Horizontal line:
-        private int hc;
+        private int hc = 5;
 
         // Linear graph:
-        private int lm;
-        private int lb;
+        private int lm = 2;
+        private int lb = 5;
 
         // Quadratic graph:
-        private int qa;
-        private int qb;
-        private int qc;
+        private int qa = 2;
+        private int qb = 3;
+        private int qc = 4;
 
         // Linear derivative graph:
-        private int ldm;
-        private int ldb;
+        private int ldm = 2;
+        private int ldb = 5;
 
         // Quadratic derivative graph:
-        private int qda;
-        private int qdb;
-        private int qdc;
+        private int qda = 2;
+        private int qdb = 3;
+        private int qdc = 4;
 
         // Circular graph:
         private int ca;
@@ -119,28 +119,43 @@ class MyPanel extends JPanel {
 
         // Graphs horizontal line:
         for (int i = 0; i < dimensions; i++) { // Draws x-axis
-            g.fillRect(i, dimensions / 2 + hc * scale, POINTWIDTH, POINTHEIGHT);
-            g.drawRect(i, dimensions / 2 + hc * scale, POINTWIDTH, POINTHEIGHT);
+            g.fillRect(i, dimensions / 2 - hc * scale, POINTWIDTH, POINTHEIGHT);
+            g.drawRect(i, dimensions / 2 - hc * scale, POINTWIDTH, POINTHEIGHT);
         }
 
         // Graphs linear graph:
         for (int x = 0; x < dimensions; x++) { // Repeats for each pixel in plane length
             point = (x - (double) (dimensions / 2)) / scale; // Converts value of each pixel on plane to a scaled-down version for the graph
-            g.fillRect((x), - (int) (point * scale * lm) + (dimensions / 2), POINTWIDTH, POINTHEIGHT);
-            g.drawRect((x), - (int) (point * scale * lm) + (dimensions / 2), POINTWIDTH, POINTHEIGHT);
+            g.fillRect((x), - (int) (point * scale * lm) + (dimensions / 2) - (lb * scale), POINTWIDTH, POINTHEIGHT);
+            g.drawRect((x), - (int) (point * scale * lm) + (dimensions / 2) - (lb * scale), POINTWIDTH, POINTHEIGHT);
         }
 
         // Graphs quadratic graph:
         for (int x = 0; x < dimensions; x++) { // Repeats for each pixel in plane length
-            point = (x - (double)(dimensions / 2)) / scale; // Converts value of each pixel on plane to a scaled-down version for the graph
-            g.fillRect((x), - (int)(point * point * scale) + (dimensions / 2), POINTWIDTH, POINTHEIGHT); // Draws a point on the graph at the correct spot (translates from plane coordinate system to panel coordinate system
-            g.drawRect((x), - (int)(point * point * scale) + (dimensions / 2), POINTWIDTH, POINTHEIGHT);
+            point = (x - (double) (dimensions / 2)) / scale; // Converts value of each pixel on plane to a scaled-down version for the graph
+            g.fillRect((x), - (int) (point * point * scale * qa) + (dimensions / 2) - (int) (point * scale * qb) - (qc * scale), POINTWIDTH, POINTHEIGHT); // Draws a point on the graph at the correct spot (translates from plane coordinate system to panel coordinate system
+            g.drawRect((x), - (int) (point * point * scale * qa) + (dimensions / 2) - (int) (point * scale * qb) - (qc * scale), POINTWIDTH, POINTHEIGHT);
+        }
 
         // Graphs linear derivative graph:
+        for (int i = 0; i < dimensions; i++) { // Draws x-axis
+            g.fillRect(i, dimensions / 2 - ldm * scale, POINTWIDTH, POINTHEIGHT);
+            g.drawRect(i, dimensions / 2 - ldm * scale, POINTWIDTH, POINTHEIGHT);
+        }
 
         // Graphs quadratic derivative graph:
+        for (int x = 0; x < dimensions; x++) { // Repeats for each pixel in plane length
+            point = (x - (double) (dimensions / 2)) / scale; // Converts value of each pixel on plane to a scaled-down version for the graph
+            g.fillRect((x), - (int) (point * scale * qda * 2) + (dimensions / 2) - (qdb * scale), POINTWIDTH, POINTHEIGHT);
+            g.drawRect((x), - (int) (point * scale * qda * 2) + (dimensions / 2) - (qdb * scale), POINTWIDTH, POINTHEIGHT);
+        }
 
         // Graphs circular graph:
+        for (int x = 0; x < dimensions; x++) { // Repeats for each pixel in plane length
+            point = (x - (double) (dimensions / 2)) / scale; // Converts value of each pixel on plane to a scaled-down version for the graph
+            g.fillRect((x), - (int) (cr * scale * qa) + (dimensions / 2) - (int) (point * scale * qb) - (qc * scale), POINTWIDTH, POINTHEIGHT); // Draws a point on the graph at the correct spot (translates from plane coordinate system to panel coordinate system
+            g.drawRect((x), - (int) (cr * scale * qa) + (dimensions / 2) - (int) (point * scale * qb) - (qc * scale), POINTWIDTH, POINTHEIGHT);
+        }
 
         // Graphs trigonometric graph:
 
@@ -155,7 +170,7 @@ class MyPanel extends JPanel {
             System.out.println(point * point * scale);
             System.out.println("");
             */
-        }
+
 
         for (int x = 0; x < dimensions; x++) { // Repeats for each pixel in plane length
             point = (x - (double) (dimensions / 2)) / scale; // Converts value of each pixel on plane to a scaled-down version for the graph
